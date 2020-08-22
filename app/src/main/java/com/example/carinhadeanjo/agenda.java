2c3991b29d69b6f4009204a1514a4318a06b0bcb
Gabriel Vilarouca <gabrielrvilarouca@hotmail.com>
2020-08-22 18:14:16 -0300

    MODELO DE ENVIO DA AGENDA
    O processo ocorre após o botão "Pronto" no final for clicado.
    Ele deverá ser implementado

--- app/src/main/java/com/example/carinhadeanjo/agenda.java
+++ app/src/main/java/com/example/carinhadeanjo/agenda.java
@@ -3,6 +3,8 @@ package com.example.carinhadeanjo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class agenda extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class agenda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+    }
+
+    String p1 = "";
+    public void enviar(View view){
+        Switch s1 = findViewById(R.id.s1);
+        Switch s2 = findViewById(R.id.s2);
+        if (s1.isChecked() == true){
+            p1 = "Português | ";
+        }
+        if (s2.isChecked() == true){
+            p1 += "Literatura | ";
+        }
     }
 }
