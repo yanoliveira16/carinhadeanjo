cf7a146175e7e60b0d80bab7bbf44c750da2104e
Gabriel Vilarouca <gabrielrvilarouca@hotmail.com>
2021-01-21 19:31:41 -0300

    1 previa | 1.5.3

---
+++ app/src/main/java/com/gabrielvilarouca/carinhadeanjo/cordena_alunos.java
@@ -0,0 +1,14 @@
+package com.gabrielvilarouca.carinhadeanjo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cordena_alunos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cordena_alunos);
+    }
+}
